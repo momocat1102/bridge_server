72f1c0e96c3ea6f9cd59cef4d38f7f5f31a34d73
Momocat <oyj674@gmail.com>
2023-04-08 03:03:57 +0800

    word color

--- src/Competition/Board.jsx
+++ src/Competition/Board.jsx
@@ -23,34 +23,51 @@ class Board extends Component {
   call_light(val, num, i) {
     let val_ary = [0, 0];
     let type = " btn_call_click";
-    if (val == 0) {
+    if (val === 0) {
       val_ary[0] = -1;
       val_ary[1] = 5;
     }
     else {
-      val_ary[0] = val % 5 == 0 ? Math.floor(val / 5) - 1 : Math.floor(val / 5);
-      val_ary[1] = val % 5 == 0 ? 4 : val % 5 - 1;
+      val_ary[0] = val % 5 === 0 ? Math.floor(val / 5) - 1 : Math.floor(val / 5);
+      val_ary[1] = val % 5 === 0 ? 4 : val % 5 - 1;
     }
-    return val_ary[i] == num ? type : "";
+    return val_ary[i] === num ? type : "";
   }
 
   call_note(rec, num, p) {
     let flower = this.flower;
     let val = "";
-    let note = "Bid: ";
+    let note = [];
+    note.push("Bid: ")
     for (let i = 0; i < rec.call.length; i++) {
-      if (i <= num && rec.call[i].player == p) {
+      if (i <= num && rec.call[i].player === p) {
         val = rec.call[i].call_val;
-        if (val == 0) {
-          note += flower[5];
+        if (val === 0) {
+          note.push(flower[5]);
         }
         else {
-          note += val % 5 == 0 ? Math.floor(val / 5) : Math.floor(val / 5) + 1;
-          note += val % 5 == 0 ? flower[4] : flower[val % 5 - 1] + "➝";
+          if(val % 5 === 0){
+            if(flower[4] === "♣" || flower[4] === "♠"){
+              note.push(<apan style={{ color: "black"}}>{Math.floor(val / 5) + flower[4]}</apan>);
+            } else if(flower[4] === "♦" || flower[4] === "♥"){
+              note.push(<apan style={{ color: "red"}}>{Math.floor(val / 5) + flower[4]}</apan>);
+            } else{
+              note.push(Math.floor(val / 5) + flower[4]);
+            }
+          } else {
+            if(flower[val % 5 - 1] === "♣" || flower[val % 5 - 1] === "♠"){
+              note.push(<apan style={{ color: "black"}}>{Math.floor(val / 5) + 1 + flower[val % 5 - 1]}</apan>);
+            } else if(flower[val % 5 - 1] === "♦" || flower[val % 5 - 1] === "♥"){
+              note.push(<apan style={{ color: "red"}}>{Math.floor(val / 5) + 1 + flower[val % 5 - 1]}</apan>);
+            } else{
+              note.push(Math.floor(val / 5) + 1 + flower[val % 5 - 1]);
+            }
+            note.push("➝");
+          }
         }
       }
     }
-    return note;
+    return <p className="txt_st">{note}</p>;
   }
 
   call_user() {
@@ -59,7 +76,7 @@ class Board extends Component {
     for (let i = 0; i < 2; i++) {
       num = Math.floor(Math.random() * 9 + 1)
       num = num < 10 ? "x0" + num.toString() : "x" + num.toString();
-      if (arr.indexOf(num) == -1) {
+      if (arr.indexOf(num) === -1) {
         arr.push(num);
       } else {
         i--;
@@ -71,7 +88,7 @@ class Board extends Component {
   change_card(rec, p, p1, p2) {
     let hand = [[...rec.hand_cards.p1], [...rec.hand_cards.p2]];
     let card = [rec.first_card, rec.first_get, rec.second_card, rec.second_get];
-    let role = (p == 0 && rec.go_first == p1) ? 0 : ((p == 1 && rec.go_first == p2) ? 0 : 2);
+    let role = (p === 0 && rec.go_first === p1) ? 0 : ((p === 1 && rec.go_first === p2) ? 0 : 2);
     hand[p].push(card[role]);
     hand[p].sort(function(a, b){return a-b});
     let indx = hand[p].indexOf(card[role]);
@@ -81,12 +98,13 @@ class Board extends Component {
 
   change_note(rec, num, p) {
     let flower = this.flower.slice(0, 4).reverse();
+    console.log(flower);
     let number = this.number;
     let card = [-1, -1];
-    let note = "";
+    let note = [];
     for (let i = 0; i < rec.change.length; i++) {
       if (i <= num) {
-        if (rec.change[i].go_first == p) {
+        if (rec.change[i].go_first === p) {
           card[0] = rec.change[i].first_card;
           card[1] = rec.change[i].first_get;
         }
@@ -94,24 +112,39 @@ class Board extends Component {
           card[0] = rec.change[i].second_card;
           card[1] = rec.change[i].second_get;
         }
-        note += flower[Math.floor(card[0] / 13)] + number[card[0] % 13] + "➝";
-        note += flower[Math.floor(card[1] / 13)] + number[card[1] % 13] + (i == rec.change.length - 1 ? "" : "..");
+        if (flower[Math.floor(card[0] / 13)] === "♣" || flower[Math.floor(card[0] / 13)] === "♠") {
+          note.push(<apan style={{ color: "black"}}>{flower[Math.floor(card[0] / 13)] + number[card[0] % 13]}</apan>);
+        } else if (flower[Math.floor(card[0] / 13)] === "♦" || flower[Math.floor(card[0] / 13)] === "♥") {
+          note.push(<apan style={{ color: "red"}}>{flower[Math.floor(card[0] / 13)] + number[card[0] % 13]}</apan>);
+        } else {
+          note.push(<apan>{flower[Math.floor(card[0] / 13)] + number[card[0] % 13]}</apan>);
+        }
+        note.push("➝");
+        if (flower[Math.floor(card[1] / 13)] === "♣" || flower[Math.floor(card[1] / 13)] === "♠") {
+          note.push(<apan style={{ color: "black"}}>{flower[Math.floor(card[1] / 13)] + number[card[1] % 13]}</apan>);
+        } else if (flower[Math.floor(card[1] / 13)] === "♦" || flower[Math.floor(card[1] / 13)] === "♥") {
+          note.push(<apan style={{ color: "red"}}>{flower[Math.floor(card[1] / 13)] + number[card[1] % 13]}</apan>);
+        } else {
+          note.push(<apan>{flower[Math.floor(card[1] / 13)] + number[card[1] % 13]}</apan>);
+        }
+        note.push(i === rec.change.length - 1 ? "" : "..");
       }
     }
-    return note;
+    console.log(note);
+    return <p className="txt_st1">{note}</p> ;
   }
 
   play_card(rec, i, p, p1, p2) {
     let hand = [[...rec.play[i].hand_cards.p1], [...rec.play[i].hand_cards.p2]];
     let card = [rec.play[i].first_play, rec.play[i].second_play];
-    let role = (p == 0 && rec.play[i].go_first == p1) ? 0 : ((p == 1 && rec.play[i].go_first == p2) ? 0 : 1);
+    let role = (p === 0 && rec.play[i].go_first === p1) ? 0 : ((p === 1 && rec.play[i].go_first === p2) ? 0 : 1);
     hand[p].push(card[role]);
     hand[p].sort(function(a, b){return a-b});
     let indx = hand[p].indexOf(card[role]);
-    if (i != 0) {
+    if (i !== 0) {
       i--;
       let card1 = [rec.play[i].first_play, rec.play[i].second_play];
-      hand[p].push(card1[(p == 0 && rec.play[i].go_first == p1) ? 0 : ((p == 1 && rec.play[i].go_first == p2) ? 0 : 1)]);
+      hand[p].push(card1[(p === 0 && rec.play[i].go_first === p1) ? 0 : ((p === 1 && rec.play[i].go_first === p2) ? 0 : 1)]);
     }
     return [hand[p], indx];
   }
@@ -120,19 +153,26 @@ class Board extends Component {
     let flower = this.flower.slice(0, 4).reverse();
     let number = this.number;
     let val = "";
-    let note = "";
+    let note = [];
     for (let i = 0; i < rec.play.length; i++) {
       if (i <= num) {
-        if (rec.play[i].go_first == p) {
+        if (rec.play[i].go_first === p) {
           val = rec.play[i].first_play;
         }
         else {
           val = rec.play[i].second_play;
         }
-        note += flower[Math.floor(val / 13)] + number[val % 13] + (i == rec.play.length - 1 ? "" : "➝");
+        if(flower[Math.floor(val / 13)] === "♣" || flower[Math.floor(val / 13)] === "♠"){
+          note.push(<apan style={{ color: "black"}}>{flower[Math.floor(val / 13)] + number[val % 13]}</apan>); 
+        } else if (flower[Math.floor(val / 13)] === "♦" || flower[Math.floor(val / 13)] === "♥") {
+          note.push(<apan style={{ color: "red"}}>{flower[Math.floor(val / 13)] + number[val % 13]}</apan>);
+        } else {
+          note.push(<apan>{flower[Math.floor(val / 13)] + number[val % 13]}</apan>);
+        }
+        note.push(i === rec.play.length - 1 ? "" : "➝")
       }
     }
-    return note;
+    return <p className="txt_st1">{note}</p>;
   }
 
   Nextplay = () => {
@@ -246,7 +286,7 @@ class Board extends Component {
                 <img src={this.images[this.user_img[0] + ".png"].default} className="img_user"></img>
                 <p className="txt_st">{record.player.p1}</p>
                 <hr className="hr_user" />
-                <p className="txt_st">{this.call_note(record, this.state.callIndex, record.player.p1)}</p>
+                {this.call_note(record, this.state.callIndex, record.player.p1)}
                 <br/>
                 <p className="txt_st">{"Time: " + record.call[this.state.callIndex].time_limit.p1}</p>
             </div>
@@ -254,7 +294,7 @@ class Board extends Component {
                 <img src={this.images[this.user_img[1] + ".png"].default} className="img_user"></img>
                 <p className="txt_st">{record.player.p2}</p>
                 <hr className="hr_user" />
-                <p className="txt_st">{this.call_note(record, this.state.callIndex, record.player.p2)}</p>
+                {this.call_note(record, this.state.callIndex, record.player.p2)}
                 <br/>
                 <p className="txt_st">{"Time: " + record.call[this.state.callIndex].time_limit.p2}</p>
             </div>
@@ -283,23 +323,23 @@ class Board extends Component {
               </button>
             ))}
             <button className="btn_card zindex_22 top_275 left_c0">
-              <img src={this.images[(this.state.changeIndex == record.change.length - 1 ? "PZ" : "PB") + ".png"].default} className="img_card"></img>
+              <img src={this.images[(this.state.changeIndex === record.change.length - 1 ? "PZ" : "PB") + ".png"].default} className="img_card"></img>
             </button>
             <button className="btn_card zindex_22 top_275 left_c2 move_nt">
-              <img src={this.images[(this.state.changeIndex == record.change.length - 1 ? "PZ" : record.change[this.state.changeIndex].first_change_card) + ".png"].default} className="img_card"></img>
+              <img src={this.images[(this.state.changeIndex === record.change.length - 1 ? "PZ" : record.change[this.state.changeIndex].first_change_card) + ".png"].default} className="img_card"></img>
             </button>
             <div className="zindex_10 top_25 left_1270 div_user">
                 <img src={this.images[this.user_img[0] + ".png"].default} className="img_user"></img>
                 <p className="txt_st">{record.player.p1}</p>
                 <hr className="hr_user" />
-                <p className="txt_st1">{this.change_note(record, this.state.changeIndex, record.player.p1)}</p>
+                {this.change_note(record, this.state.changeIndex, record.player.p1)}
                 <p className="txt_st">{"Time: " + record.change[this.state.changeIndex].time_limit.p1}</p>
             </div>
             <div className="zindex_10 top_380 left_1270 div_user">
                 <img src={this.images[this.user_img[1] + ".png"].default} className="img_user"></img>
                 <p className="txt_st">{record.player.p2}</p>
                 <hr className="hr_user" />
-                <p className="txt_st1">{this.change_note(record, this.state.changeIndex, record.player.p2)}</p>
+                {this.change_note(record, this.state.changeIndex, record.player.p2)}
                 <p className="txt_st">{"Time: " + record.change[this.state.changeIndex].time_limit.p2}</p>
             </div>
             <div className={"zindex_22 div_trump top_" + (record.dealer == p1 ? 10 : 365).toString() + " left_1260"}>
@@ -318,26 +358,26 @@ class Board extends Component {
           <>
             {this.play_card(record, this.state.playIndex, 0, p1, p2)[0].map((card, id) => (
               change = this.play_card(record, this.state.playIndex, 0, p1, p2)[1],
-              btn_style = (this.state.playIndex != 0 && id == this.play_card(record, this.state.playIndex, 0, p1, p2)[0].length - 1) ? "btn_card zindex_10 top_240 left_c8 move_ns" : "btn_card zindex_" + (22 - id).toString() + " top_25 left_c" + id.toString() + (id == change ? " move_co" : ""),
+              btn_style = (this.state.playIndex !== 0 && id === this.play_card(record, this.state.playIndex, 0, p1, p2)[0].length - 1) ? "btn_card zindex_10 top_240 left_c8 move_ns" : "btn_card zindex_" + (22 - id).toString() + " top_25 left_c" + id.toString() + (id == change ? " move_co" : ""),
               <button className={btn_style}>
                 <img src={this.images[card + ".png"].default} className="img_card" alt="card"></img>
               </button>
             ))}
             {this.play_card(record, this.state.playIndex, 1, p1, p2)[0].map((card1, id1) => (
               change = this.play_card(record, this.state.playIndex, 1, p1, p2)[1],
-              btn_style = (this.state.playIndex != 0 && id1 == this.play_card(record, this.state.playIndex, 1, p1, p2)[0].length - 1) ? "btn_card zindex_10 top_310 left_c11 move_ns" : "btn_card zindex_" + (10 + id1).toString() + " top_525 left_c" + id1.toString() + (id1 == change ? " move_po" : ""),
+              btn_style = (this.state.playIndex !== 0 && id1 === this.play_card(record, this.state.playIndex, 1, p1, p2)[0].length - 1) ? "btn_card zindex_10 top_310 left_c11 move_ns" : "btn_card zindex_" + (10 + id1).toString() + " top_525 left_c" + id1.toString() + (id1 == change ? " move_po" : ""),
               <button className={btn_style}>
                 <img src={this.images[card1 + ".png"].default} className="img_card" alt="card"></img>
               </button>
             ))}
             <button className="btn_card zindex_22 top_275 left_c0">
-              <img src={this.images[(this.state.playIndex == 0 ? "PZ" : "PB") + ".png"].default} className="img_card"></img>
+              <img src={this.images[(this.state.playIndex === 0 ? "PZ" : "PB") + ".png"].default} className="img_card"></img>
             </button>
             <div className="zindex_10 top_25 left_1270 div_user">
                 <img src={this.images[this.user_img[0] + ".png"].default} className="img_user"></img>
                 <p className="txt_st">{record.player.p1}</p>
                 <hr className="hr_user" />
-                <p className="txt_st1">{this.play_note(record, this.state.playIndex, record.player.p1)}</p>
+                {this.play_note(record, this.state.playIndex, record.player.p1)}
                 <p className="txt_st">{"Score: " + record.play[this.state.playIndex].score.p1}</p>
                 <p className="txt_st">{"Time: " + record.play[this.state.playIndex].time_limit.p1}</p>
             </div>
@@ -345,11 +385,11 @@ class Board extends Component {
                 <img src={this.images[this.user_img[1] + ".png"].default} className="img_user"></img>
                 <p className="txt_st">{record.player.p2}</p>
                 <hr className="hr_user" />
-                <p className="txt_st1">{this.play_note(record, this.state.playIndex, record.player.p2)}</p>
+                {this.play_note(record, this.state.playIndex, record.player.p1)}
                 <p className="txt_st">{"Score: " + record.play[this.state.playIndex].score.p2}</p>
                 <p className="txt_st">{"Time: " + record.play[this.state.playIndex].time_limit.p2}</p>
             </div>
-            <div className={"zindex_22 div_trump top_" + (record.dealer == p1 ? 10 : 365).toString() + " left_1260"}>
+            <div className={"zindex_22 div_trump top_" + (record.dealer === p1 ? 10 : 365).toString() + " left_1260"}>
               <img src={this.images["t" + record.trump[0].toString() + ".png"].default} className="img_trump1"></img>
               <img src={this.images["t" + btn_trump[2][record.trump[1]].toString() + ".png"].default} className="img_trump"></img>
             </div>
